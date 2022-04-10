19415d46005b4677950948767beed5e4b50fa5e8
Taavi Aun <auntaavi@gmail.com>
2022-04-10 17:37:34 +0300

    Add custom 404 page

---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <section>
+      <img src="/catshome/assets/img/404_cat.svg" alt="not found page cat" />
+    </section>
+    <section>
+      <h1>Page not found</h1>
+    </section>
+    <section>
+      <a href="/catshome/">Go to home page</a>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 90vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+section {
+  display: flex;
+}
+
+img {
+  height: 30vh;
+}
+</style>
