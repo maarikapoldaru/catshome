938b58bd11350a1bd954e6cde28dadd5253556d9
kpuistaja <Kadi.Puistaja@tptlive.ee>
2022-04-30 17:32:16 +0300

    Run prettier

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,15 +1,15 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faFacebook, faInstagram)
+library.add(faFacebook, faInstagram);
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
+  siteData, // site metadata
 }) => {
   // ...apply enhancements to the app
-  Vue.component('font-awesome-icon', FontAwesomeIcon)
-}
+  Vue.component("font-awesome-icon", FontAwesomeIcon);
+};
--- docs/.vuepress/theme/layouts/GlobalLayout.vue
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -11,16 +11,16 @@
           <div class="columns">
             <div class="column">
               <h4>Kontakt</h4>
-              <p>info@kurrumurru.ee<br>+372 1234 5678<br>Kassi tee, Kurru Murru saar</p>
+              <p>info@kurrumurru.ee<br />+372 1234 5678<br />Kassi tee, Kurru Murru saar</p>
             </div>
             <div class="column">
               <h4>Annetamine</h4>
-              <p>Swed EE12345678912345678<br>SEB EE12345678912345678<br>LHV EE12345678912345678</p>
+              <p>Swed EE12345678912345678<br />SEB EE12345678912345678<br />LHV EE12345678912345678</p>
             </div>
             <div class="column">
               <h4>Sotsiaalmeedia</h4>
-                <a class="icon" href="https://www.instagram.com"><font-awesome-icon icon="fab fa-instagram" /></a>
-                <a class="icon" href="https://www.facebook.com"><font-awesome-icon icon="fa-brands fa-facebook" /></a>
+              <a class="icon" href="https://www.instagram.com"><font-awesome-icon icon="fab fa-instagram" /></a>
+              <a class="icon" href="https://www.facebook.com"><font-awesome-icon icon="fa-brands fa-facebook" /></a>
             </div>
           </div>
           <div>
@@ -41,8 +41,7 @@ import GlobalLayout from "@vuepress/core/lib/client/components/GlobalLayout.vue"
 
 export default {
   components: {
-    GlobalLayout
-  }
+    GlobalLayout,
+  },
 };
 </script>
-
