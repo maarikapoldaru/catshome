e5e2dcc6ce3f1cefc609946c182aa5088f5d9f59
eritikass <eritikass@users.noreply.github.com>
2022-05-07 11:56:58 +0000

    Auto deploy from Github Actions

---
+++ assets/js/20.595bd05f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{504:function(t,e,s){"use strict";s.r(e);var n=s(34),l=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("lorem ipsum")])])}),[],!1,null,null,null);e.default=l.exports}}]);
